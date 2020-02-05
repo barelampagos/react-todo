5f6566b13d07408927ca847c941a32615d3dc20b
barelampagos <barelampagos@gmail.com>
2020-02-04 20:32:07 -0800

    Updating filterTodos with text & sort

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -57,7 +57,7 @@ describe('TodoAPI', () => {
 		var todos = [
 			{ id: 1, text: 'Do something', completed: true },
 			{ id: 2, text: 'Do something else', completed: false },
-			{ id: 3, text: 'Do something elser', completed: true }
+			{ id: 3, text: 'Do that thing', completed: true }
 		];
 
 		it('Should return all items if showCompleted is true', () => {
@@ -67,6 +67,21 @@ describe('TodoAPI', () => {
 
 		it('Should return one items if showCompleted is false', () => {
 			var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+			expect(filteredTodos.length).toBe(1);
+		});
+
+		it('Should sort by completed status', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filteredTodos[0].completed).toBe(false);
+		});
+
+		it('Should return all todos when searchText empty', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+			expect(filteredTodos.length).toBe(3);
+		});
+
+		it('Should return only todos that match searchText', () => {
+			var filteredTodos = TodoAPI.filterTodos(todos, true, 'that');
 			expect(filteredTodos.length).toBe(1);
 		});
 	});
