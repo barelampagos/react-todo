73dde49a489570bf7e12cbbb58761e946411e8e3
barelampagos <barelampagos@gmail.com>
2020-03-14 16:40:14 -0700

    Adding todo reducers + support for spread object parsing

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -6,16 +6,16 @@ export var setSearchText = searchText => {
 	};
 };
 
+export var toggleShowCompleted = () => {
+	return {
+		type: 'TOGGLE_SHOW_COMPLETED'
+	};
+};
+
 export var addTodo = text => {
 	return {
 		type: 'ADD_TODO',
 		text
-	};
-};
-
-export var toggleShowCompleted = () => {
-	return {
-		type: 'TOGGLE_SHOW_COMPLETED'
 	};
 };
 
